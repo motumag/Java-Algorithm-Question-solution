dacf025a9f00ee980ffe05e0ff6e01a0e350b58a
motumag <motumagishu27@gmail.com>
2023-05-24 13:50:30 +0300

    The armstrong number is done

--- src/SimpleMaths/CheckIfNumberIsArmstrong.java
+++ src/SimpleMaths/CheckIfNumberIsArmstrong.java
@@ -1,7 +1,24 @@
 package SimpleMaths;
 
 public class CheckIfNumberIsArmstrong {
+    int isArmstrong(int n){
+        int sum=0;
+        int originalNumber=n;
+        while(n>0){
+            int ld=n%10;
+            n=n/10;
+            sum= sum+(int) Math.pow(ld,3);
+        }
+        if(originalNumber==sum){
+            System.out.println("The number is Armstrong");
+        }else {
+            System.out.println("Not armstrong");
+        }
+        return sum;
+    }
     public static void main(String[] args) {
+        CheckIfNumberIsArmstrong armstrong=new CheckIfNumberIsArmstrong();
+        int checkArm=armstrong.isArmstrong(371);
 
     }
 }
